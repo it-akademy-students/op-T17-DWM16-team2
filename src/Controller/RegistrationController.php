214ac05ffd895c14465d4329ff45371fd7a5570a
Pierre-Louis <pl.nitard@it-students.fr>
2021-12-08 13:03:04 +0000

    Système de connexion/création AccountController / Modif navbar

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -16,6 +16,10 @@ class RegistrationController extends AbstractController
     #[Route('/register', name: 'app_register')]
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
     {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('account');
+        }
+        
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
@@ -38,7 +42,7 @@ class RegistrationController extends AbstractController
             $entityManager->flush();
             // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('home');
+            return $this->redirectToRoute('account');
         }
 
         return $this->render('registration/register.html.twig', [
