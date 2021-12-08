a5edc2bf76f44c42ae77856cd45cf5bfe3b92e39
Pierre-Louis <pl.nitard@it-students.fr>
2021-12-08 15:44:27 +0000

    Movie page upgraded

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -7,19 +7,21 @@ use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Service\CallApiService;
 
 class MovieController extends AbstractController
 {
     #[Route('/movie/{id}', name: 'movie')]
-    public function index(ManagerRegistry $doctrine, int $id): Response
+    public function index(ManagerRegistry $doctrine, int $id, CallApiService $callApiService): Response
     {
         $movie = $doctrine->getRepository(Movie::class)->find($id);
 
         if (!$movie) {
-            throw $this->createNotFoundException(
-                'No movie found for id '.$id
-            );
+            // Si le film n'est pas trouvé, alors on redirige vers la page d'accueil
+            return $this->redirectToRoute('home');
         }
+
+        $movie = $callApiService->getMovieData($movie->getImdbId());
 
         return $this->render('movie/index.html.twig', [
             'movie' => $movie,
