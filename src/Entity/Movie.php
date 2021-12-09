7478eb2de6c4b9fbbdc4b009679e94ba70c090a6
balde24 <t.balde@it-students.fr>
2021-12-09 11:32:44 +0100

    create cartcontroller

---
+++ src/Entity/Movie.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MovieRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=MovieRepository::class)
+ */
+class Movie
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $imdb_id;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $price;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getImdbId(): ?string
+    {
+        return $this->imdb_id;
+    }
+
+    public function setImdbId(string $imdb_id): self
+    {
+        $this->imdb_id = $imdb_id;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+}
