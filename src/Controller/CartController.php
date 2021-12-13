bdb96d1d4eac95d0dd07ed0da7824dde80c17f6d
unknown <21266356+plntd@users.noreply.github.com>
2021-12-13 15:27:47 +0100

    Added simulation API in cart controller

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -21,7 +21,16 @@ class CartController extends AbstractController
      
         foreach($panier as $id => $quantity) {
             $panierWithData[] = [
-                'movie' => $movierepository->find($id),
+                'movie' => [
+                    'movie' => $movierepository->find($id),
+                    0 => [
+                        'title' => 'Inception',
+                        'year' => 2021,
+                        'plotLocal' => 'Dom Cobb est un voleur expérimenté, le meilleur dans l\'art dangereux de l\'extraction, voler les secrets les plus intimes enfouis au plus profond du subconscient durant une phase de rêve, lorsque l\'esprit est le plus vulnérable. Les capacités de Cobb ont fait des envieux dans le monde tourmenté de l\'espionnage industriel alors qu\'il devient fugitif en perdant tout ce qu\'il a un jour aimé. Une chance de se racheter lui est alors offerte. Une ultime mission grâce à laquelle il pourrait retrouver sa vie passée mais uniquement s\'il parvient à accomplir l\'impossible inception.',
+                        'imDbRating' => 8.3,
+                        'image' => 'https://imdb-api.com/images/original/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_Ratio0.6762_AL_.jpg'
+                    ]
+                ],
                 'quantity' => $quantity
             ];
         }
@@ -29,7 +38,7 @@ class CartController extends AbstractController
         $total = 0;
 
         foreach($panierWithData as $item) {
-            $totalItem = $item['movie']->getPrice() * $item['quantity'];
+            $totalItem = $item['movie']['movie']->getPrice() * $item['quantity'];
             $total += $totalItem;
         }
 
